036f241f76a2fe4de1309354dd71f887fb0e8ff1
Mike Torres <apolloproductionsdj@gmail.com>
2020-08-03 18:56:56 +0000

    added carrierwave and switched it to aws storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_02_065841) do
+ActiveRecord::Schema.define(version: 2020_08_03_182757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_08_02_065841) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "picture"
     t.index ["user_id"], name: "index_grams_on_user_id"
   end
 
